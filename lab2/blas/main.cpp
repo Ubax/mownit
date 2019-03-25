691d23ede4c5e7d9ff2a417696e4facdc77552ae
Ubax <32908614+Ubax@users.noreply.github.com>
2019-03-25 11:54:21 +0100

    Results for ddot and demv

--- lab2/blas/main.cpp
+++ lab2/blas/main.cpp
@@ -12,11 +12,13 @@ gsl_vector *random_vector(size_t length) {
 }
 
 gsl_matrix *random_matrix(size_t length) {
-    gsl_vector *vec1 = gsl_vector_alloc(length);
+    gsl_matrix *matrix = gsl_matrix_alloc(length, length);
     for (size_t i = 0; i < length; i++) {
-        gsl_vector_set(vec1, i, rand() / RAND_MAX);
+        for (size_t j = 0; j < length; j++) {
+            gsl_matrix_set(matrix, i, j, rand() / RAND_MAX);
+        }
     }
-    return vec1;
+    return matrix;
 }
 
 double* ddot(size_t array_size, size_t vector_length) {
@@ -24,18 +26,26 @@ double* ddot(size_t array_size, size_t vector_length) {
     gsl_vector *vec1 = random_vector(vector_length);
     gsl_vector *vec2 = random_vector(vector_length);
     gsl_blas_ddot(vec1, vec2, res);
+    gsl_vector_free(vec1);
+    gsl_vector_free(vec2);
     return res;
 }
 
-double *dgemv(size_t array_size, size_t vector_length){
-    gsl_vector *vec1 = random_vector(vector_length);
-    gsl_vector *vec2 = random_vector(vector_length);
-    gsl_blas_dgemv(CblasNoTrans, 1, ,,1);
+void dgemv(size_t array_size, size_t vector_length){
+    gsl_matrix *A = random_matrix(vector_length);
+    gsl_vector *X = random_vector(vector_length);
+    gsl_vector *Y = random_vector(vector_length);
+    gsl_blas_dgemv(CblasNoTrans, 1, A, X,1,Y);
+    gsl_vector_free(X);
+    gsl_vector_free(Y);
+    gsl_matrix_free(A);
 }
 
 int main() {
     timespec start, stop;
-    for(int j=10;j<10000000;j*=100){
+    printf("Vector x Vector (ddot)\n");
+    for(int j=10;j<10000000;j*=10){
+        double sum=0.0;
         for(int i=0;i<10;i++) {
             if (clock_gettime(CLOCK_REALTIME, &start) == -1) {
                 printf("Problem with clock\n");
@@ -48,7 +58,29 @@ int main() {
             }
             double dur = stop.tv_sec - start.tv_sec + (stop.tv_nsec - start.tv_nsec)*1.0/1000000000;
             printf("%lf,",dur);
+            sum+=dur;
         }
+        sum/=10;
+        printf("%i\n",j);
+    }
+    printf("Matrix x Vector (dgemv)\n");
+    for(int j=100;j<10000;j*=3){
+        double sum=0.0;
+        for(int i=0;i<10;i++) {
+            if (clock_gettime(CLOCK_REALTIME, &start) == -1) {
+                printf("Problem with clock\n");
+                return 1;
+            }
+            dgemv(j, j);
+            if (clock_gettime(CLOCK_REALTIME, &stop) == -1) {
+                printf("Problem with clock\n");
+                return 1;
+            }
+            double dur = stop.tv_sec - start.tv_sec + (stop.tv_nsec - start.tv_nsec)*1.0/1000000000;
+            printf("%lf,",dur);
+            sum+=dur;
+        }
+        sum/=10;
         printf("%i\n",j);
     }
 
